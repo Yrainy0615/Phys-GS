fa8d6f34e4ae7328456f418577964abe41dd5089
Yrainy0615 <yrainy0615@outlook.com>
2026-03-02 18:27:39 +0900

    update

--- run.sh
+++ run.sh
@@ -1,17 +1,16 @@
 # 1) extract semantic cache
 python semantic/extract_dino_semantic_features.py \
   --base_path /home/yang/Projects/Phys-GS/data/different_types \
-  # --cases single_push_sloth single_lift_zebra single_lift_cloth \
   --cases $(python - <<'PY'
-      import json
-      p='/home/yang/Projects/Phys-GS/semantic/case_to_material_different_types.json'
-      d=json.load(open(p))
-      m=d["case_to_material"] if "case_to_material" in d else d
-      print(" ".join(sorted(m.keys())))
-      PY
+import json
+p='/home/yang/Projects/Phys-GS/semantic/case_to_material_different_types.json'
+d=json.load(open(p))
+print(" ".join(sorted(d["case_to_material"].keys())))
+PY
 ) \
   --output_dir /home/yang/Projects/Phys-GS/semantic/cache \
-  --device cuda
+  --device cuda \
+  --frame_idx 0
 
 # 2) train paramnet (k-only)
 python semantic/train_paramnet_konly.py \
